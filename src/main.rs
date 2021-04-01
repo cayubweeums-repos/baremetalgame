e99be9375c40152a79c4c70e3f8e9e0401972a60
Caleb <farmingtoncardinals24@gmail.com>
2021-04-01 11:17:59 -0500

    Implemented main game functionality

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 #![no_std]
 #![no_main]
 
+// To run paste into terminal: qemu-system-x86_64 -drive format=raw,file=target\x86_64-blog_os\debug\bootimage-baremetalgame.bin -L "c:\Program files\qemu"
+
 use lazy_static::lazy_static;
 use spin::Mutex;
 use pc_keyboard::{DecodedKey, KeyCode};
 use pluggable_interrupt_os::HandlerTable;
 use pluggable_interrupt_os::vga_buffer::clear_screen;
-use baremetalgame::LetterMover;
+use baremetalgame::MainGame;
+use baremetalgame::game_main::BarrelGame;
 use crossbeam::atomic::AtomicCell;
 use pluggable_interrupt_os::println;
 
@@ -15,28 +18,18 @@ pub extern "C" fn _start() -> ! {
     HandlerTable::new()
         .keyboard(key)
         .timer(tick)
-        .startup(startup)
         .start()
 }
 
 lazy_static! {
-    static ref LETTERS: Mutex<LetterMover> = Mutex::new(LetterMover::new());
-    static ref LAST_KEY: AtomicCell<Option<DecodedKey>> = AtomicCell::new(None);
+    static ref GAME: Mutex<MainGame> = Mutex::new(BarrelGame::new());
 }
 
 fn tick() {
-    let mut letters = LETTERS.lock();
-    match LAST_KEY.swap(None) {
-        None => {}
-        Some(key) => letters.key(key)
-    }
-    letters.tick();
+    baremetalgame::tick(&mut GAME.lock())
 }
 
 fn key(key: DecodedKey) {
-    LAST_KEY.store(Some(key));
-}
-
-fn startup() {
-    clear_screen();
+    let mut game = GAME.lock();
+    game.key(key);
 }
